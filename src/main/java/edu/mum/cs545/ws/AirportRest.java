3a5433882bf57f93314f1120a2059ab2ea438cac
abdelrahman91 <38641993+abdelrahman91@users.noreply.github.com>
2018-06-19 06:02:35 -0500

    Commenting the Airport Code

--- src/main/java/edu/mum/cs545/ws/AirportRest.java
+++ src/main/java/edu/mum/cs545/ws/AirportRest.java
@@ -17,27 +17,27 @@ public class AirportRest {
 	@Inject
 	private AirlineService airlineService;
 
-	@POST
-	@Path("/create")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response postStudentRecord(Airline airline){
+//	@POST
+//	@Path("/create")
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Response postStudentRecord(Airline airline){
+//
+//		airlineService.create(airline);
+//		String result = "Record entered: "+ airline;
+//
+//		return Response.status(201).entity(result).build();
+//
+//	}
 
-		airlineService.create(airline);
-		String result = "Record entered: "+ airline;
-
-		return Response.status(201).entity(result).build();
-
-	}
 
 
-
-	@Path("/allAirLines")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Airline> getAllAirLines() {
-
-		return airlineService.findAll();
-	}
+//	@Path("/allAirLines")
+//	@GET
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public List<Airline> getAllAirLines() {
+//
+//		return airlineService.findAll();
+//	}
 
 }
