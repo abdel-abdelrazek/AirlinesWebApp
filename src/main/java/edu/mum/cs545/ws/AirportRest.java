d07ba376639aa737d8d87e9a9900bfbec817499c
abdelrahman91 <38641993+abdelrahman91@users.noreply.github.com>
2018-06-19 06:38:36 -0500

    Commenting the AirportRest WS

--- src/main/java/edu/mum/cs545/ws/AirportRest.java
+++ src/main/java/edu/mum/cs545/ws/AirportRest.java
@@ -1,43 +1,43 @@
-package edu.mum.cs545.ws;
-
-import cs545.airline.model.Airline;
-import cs545.airline.service.AirlineService;
-
-import javax.inject.Inject;
-import javax.inject.Named;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.List;
-
-@Named
-@Path("airline")
-public class AirportRest {
-
-	@Inject
-	private AirlineService airlineService;
-
-//	@POST
-//	@Path("/create")
-//	@Consumes(MediaType.APPLICATION_JSON)
-//	@Produces(MediaType.APPLICATION_JSON)
-//	public Response postStudentRecord(Airline airline){
+//package edu.mum.cs545.ws;
 //
-//		airlineService.create(airline);
-//		String result = "Record entered: "+ airline;
+//import cs545.airline.model.Airline;
+//import cs545.airline.service.AirlineService;
 //
-//		return Response.status(201).entity(result).build();
+//import javax.inject.Inject;
+//import javax.inject.Named;
+//import javax.ws.rs.*;
+//import javax.ws.rs.core.MediaType;
+//import javax.ws.rs.core.Response;
+//import java.util.List;
 //
-//	}
-
-
-
-//	@Path("/allAirLines")
-//	@GET
-//	@Produces(MediaType.APPLICATION_JSON)
-//	public List<Airline> getAllAirLines() {
+//@Named
+//@Path("airline")
+//public class AirportRest {
 //
-//		return airlineService.findAll();
-//	}
-
-}
+//	@Inject
+//	private AirlineService airlineService;
+//
+////	@POST
+////	@Path("/create")
+////	@Consumes(MediaType.APPLICATION_JSON)
+////	@Produces(MediaType.APPLICATION_JSON)
+////	public Response postStudentRecord(Airline airline){
+////
+////		airlineService.create(airline);
+////		String result = "Record entered: "+ airline;
+////
+////		return Response.status(201).entity(result).build();
+////
+////	}
+//
+//
+//
+////	@Path("/allAirLines")
+////	@GET
+////	@Produces(MediaType.APPLICATION_JSON)
+////	public List<Airline> getAllAirLines() {
+////
+////		return airlineService.findAll();
+////	}
+//
+//}
