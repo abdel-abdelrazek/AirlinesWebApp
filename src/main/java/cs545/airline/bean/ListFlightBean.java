d207b92fd2c1c7fbee976113dae17ff5122d89b2
abdelrahman91 <38641993+abdelrahman91@users.noreply.github.com>
2018-06-19 10:33:14 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/abdelrahman91/AirlinesWebApp""
    This reverts commit 8ce1373ac6130a1390fbd527595314864768a497.

---
+++ src/main/java/cs545/airline/bean/ListFlightBean.java
@@ -0,0 +1,138 @@
+package cs545.airline.bean;
+
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+//import java.util.stream.Collectors;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import cs545.airline.model.Airline;
+import cs545.airline.model.Airport;
+import cs545.airline.model.Flight;
+import cs545.airline.service.AirlineService;
+import cs545.airline.service.AirportService;
+import cs545.airline.service.FlightService;
+
+@Named("listFlightBean")
+@SessionScoped
+public class ListFlightBean implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT,
+			Locale.US);
+	private static DateFormat tf = DateFormat.getTimeInstance(DateFormat.SHORT,
+			Locale.US);
+	
+	private long airlineId;
+	
+	private Date departureDate;
+	private Date departureTime;
+	
+	private long destinationId;
+	
+	private List<Flight> flights;
+	
+	@Inject
+	private FlightService flightService;
+	
+	@Inject
+	private AirportService airportService;
+	
+	@Inject
+	private AirlineService airlineService;
+	
+	public void getAllFlights() {
+		flights = flightService.findAll();
+	}
+	
+	public List<Airline> getAirlines() {
+		List<Airline> all = airlineService.findAll();
+		Airline searchAll = new Airline();
+		searchAll.setId(-1);
+		searchAll.setName("All");
+		all.add(searchAll);
+		return all;
+	}
+	
+	public List<Airport> getAirports() {
+		List<Airport> all = airportService.findAll();
+		Airport searchAll = new Airport();
+		searchAll.setId(-1);
+		searchAll.setName("All");
+		all.add(searchAll);
+		return all;
+	}
+
+	public long getAirlineId() {
+		return airlineId;
+	}
+
+	public void setAirlineId(long airlineId) {
+		this.airlineId = airlineId;
+	}
+
+	public Date getDepartureDate() {
+		return departureDate;
+	}
+
+	public void setDepartureDate(Date departureDate) {
+		this.departureDate = departureDate;
+	}
+
+	public long getDestinationId() {
+		return destinationId;
+	}
+
+	public void setDestinationId(long destinationId) {
+		this.destinationId = destinationId;
+	}
+
+	public Date getDepartureTime() {
+		return departureTime;
+	}
+
+	public void setDepartureTime(Date departureTime) {
+		this.departureTime = departureTime;
+	}
+	
+	public void search() {
+//		this.getAllFlights();
+//		if (this.airlineId != -1) {
+//			this.flights = flights.stream().filter(f -> f.getAirline().getId() == this.airlineId).collect(Collectors.toList());
+//		}
+//		if (this.destinationId != -1) {
+//			this.flights = flights.stream().filter(f -> f.getDestination().getId() == this.destinationId).collect(Collectors.toList());
+//		}
+//		if (this.departureDate != null) {
+//			this.flights = flights.stream().filter(f -> f.getDepartureDate().equals(df.format(this.departureDate))).collect(Collectors.toList());
+//		}
+//		if (this.departureTime != null) {
+//			this.flights = flights.stream().filter(f -> f.getDepartureTime().equals(tf.format(this.departureTime))).collect(Collectors.toList());
+//		}
+	}
+	
+	public void clear() {
+		System.out.println("ListFlightBean.clear()");
+		this.airlineId = -1;
+		this.destinationId = -1;
+		this.departureDate = null;
+		this.departureTime = null;
+		this.getAllFlights();
+	}
+
+	public List<Flight> getFlights() {
+		return flights;
+	}
+
+	public void setFlights(List<Flight> flights) {
+		this.flights = flights;
+	}
+	
+}
