b9ac667c7c5596a8b95aae98121541b86a7fea75
jawad <jawad.turk@hotmail.com>
2018-06-18 22:01:08 -0500

    added air port rest

---
+++ src/main/java/edu/mum/cs545/ws/AirportRest.java
@@ -0,0 +1,43 @@
+package edu.mum.cs545.ws;
+
+import cs545.airline.model.Airline;
+import cs545.airline.service.AirlineService;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Named
+@Path("airline")
+public class AirportRest {
+
+	@Inject
+	private AirlineService airlineService;
+
+	@POST
+	@Path("/create")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response postStudentRecord(Airline airline){
+
+		airlineService.create(airline);
+		String result = "Record entered: "+ airline;
+
+		return Response.status(201).entity(result).build();
+
+	}
+
+
+
+	@Path("/allAirLines")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Airline> getAllAirLines() {
+
+		return airlineService.findAll();
+	}
+
+}
