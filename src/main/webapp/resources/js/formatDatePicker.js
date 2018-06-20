46a9868500f6ef41cd64c905be976fe1a0042e2d
abdelrahman91 <38641993+abdelrahman91@users.noreply.github.com>
2018-06-19 21:09:30 -0500

    Filtering Structure

---
+++ src/main/webapp/resources/js/formatDatePicker.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(".datepicker").datepicker({
+        dateFormat: 'dd MM, yy',
+        changeMonth: true,
+        changeYear: true,
+        yearRange: "1960:2017"
+    });
+});
